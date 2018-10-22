98878580cf9bf96b35759b36000707061764e294
RHR <renee.h.reynolds@gmail.com>
2018-10-21 17:48:14 -0700

    10.21.18 draft

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,8 @@
-module.exports = [{
-  "name":"Ahmed",
-  "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-  "scores":[
+module.exports = [
+  {
+    "name": "Robert",
+    "photo": "https://en.wikipedia.org/wiki/Link_(The_Legend_of_Zelda)#/media/File:Wakerlink.jpg",
+    "scores": [
       5,
       1,
       4,
@@ -13,4 +14,69 @@ module.exports = [{
       4,
       1
     ]
-}]
+  },
+  {
+    "name": "Keith",
+    "photo": "https://getlevelten.com/sites/default/files/styles/800x450/public/content/blog/images/star-trek-1.jpg?itok=EP6r8CLT&timestamp=1368737205",
+    "scores": [
+      3,
+      2,
+      1,
+      4,
+      3,
+      1,
+      2,
+      4,
+      5,
+      2
+    ]
+  },
+  {
+    "name": "Berte",
+    "photo": "https://vignette.wikia.nocookie.net/marvelcinematicuniverse/images/e/e9/Iron_Man_AIW_Profile.jpg/revision/latest/scale-to-width-down/380?cb=20180518212029",
+    "scores": [
+      2,
+      3,
+      4,
+      5,
+      1,
+      3,
+      5,
+      2,
+      4,
+      5
+    ]
+  },
+  {
+    "name": "Ben",
+    "photo": "https://vignette.wikia.nocookie.net/fantendo/images/1/18/Blurb_mparty6_mario_20080911.jpg/revision/latest/scale-to-width-down/414?cb=20130901123249",
+    "scores": [
+      3,
+      2,
+      1,
+      3,
+      5,
+      2,
+      2,
+      3,
+      2,
+      5
+    ]
+  },
+  {
+    "name": "Maddie",
+    "photo": "https://s3-us-west-2.amazonaws.com/atthisstage/wp-content/uploads/2010/01/07100701/pee-wee_1-237x300.jpg",
+    "scores": [
+      5,
+      1,
+      4,
+      2,
+      4,
+      1,
+      2,
+      4,
+      2,
+      4
+    ]
+  },
+]
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,3 +2,19 @@
 
 // A GET route with the url /api/friends. This will be used to display a JSON of all possible friends.
 // A POST routes /api/friends. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
+////////////////////////
+//
+//  HTML GET Router to "/api/friends" page
+//
+////////////////////////
+module.exports = function (app) {
+    app.get("/api/friends", function (req, res) {
+        res.sendFile(path.join(__dirname, "/api/friends.html"));
+    });
+
+    // A default, catch-all route that leads to home.html which displays the home page.
+    app.get("*", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/home.html"));
+    });
+};
+
