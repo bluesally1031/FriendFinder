c838bbdb7083cd2eb56b55244d95b29ecaead216
RHR <renee.h.reynolds@gmail.com>
2018-10-24 16:50:11 -0700

    my final attempt 10.24.18

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,13 +1,15 @@
-// Your apiRoutes.js file should contain two routes:
-
+////////////////////////
+//  Dependencies 
+////////////////////////
 var path = require("path");
-// A GET route with the url /api/friends. This will be used to display a JSON of all possible friends.
+
 // A POST routes /api/friends. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
+
 ////////////////////////
-//
 //  HTML GET Router to "/api/friends" page
-//
 ////////////////////////
+
+//A GET route with the url /api/friends. This will be used to display a JSON of all possible friends.
 module.exports = function (app) {
     app.get("/api/friends", function (req, res) {
         res.sendFile(path.join(__dirname, "/api/friends.html"));
